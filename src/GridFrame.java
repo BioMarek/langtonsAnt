ae0b95d2de4a3a91b03201885f48d4a83f6d0748
marek <marekkucerabio@gmail.com>
2022-08-07 20:19:57 +0200

    initial commit

--- src/GridFrame.java
+++ src/GridFrame.java
@@ -1,9 +1,9 @@
 import javax.swing.*;
 
-public class GridFrame extends JFrame {
+public class GridFrame extends JFrame  {
     public GridFrame() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.add(new GridPanel());
+        this.add(new GridPanel(500));
         this.setLocationRelativeTo(null);
         this.setResizable(false);
         this.setVisible(true);
--- src/GridPanel.java
+++ src/GridPanel.java
@@ -4,16 +4,25 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class GridPanel extends JPanel implements ActionListener {
-    private final int SIZE_IN_PIXELS = 750;
-    public GridPanel() {
+    int SIZE_IN_PIXELS = 750;
+    private int delay;
+
+    public GridPanel(int delay) {
+        this.delay = delay;
+
         this.setPreferredSize(new Dimension(SIZE_IN_PIXELS, SIZE_IN_PIXELS));
-        this.setBackground(Color.BLACK);
+        this.setBackground(Color.black);
         this.setFocusable(true);
     }
 
+    public void startGame() {
+        Timer timer = new Timer(delay, this);
+        timer.start();
+    }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
+    public void actionPerformed(ActionEvent actionEvent) {
+
         repaint();
     }
 }
