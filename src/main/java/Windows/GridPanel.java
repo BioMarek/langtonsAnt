0e4ce7dd65c97870df265ccc58ef96d6ed49064b
marek <marekkucerabio@gmail.com>
2022-08-10 21:35:39 +0200

    improved colors

--- src/main/java/Windows/GridPanel.java
+++ src/main/java/Windows/GridPanel.java
@@ -66,6 +66,16 @@ public class GridPanel extends JPanel implements ActionListener {
      * @param color grid number
      */
     private void setColor(int color) {
+        Color VIOLET = new Color(127, 0, 255);
+        Color BROWN = new Color(139, 69, 19);
+        Color LIGHT_GREEN = new Color(128, 255, 0);
+        Color LIGHT_BLUE = new Color(0, 128, 255);
+        Color OLIVE = new Color(128, 128, 0);
+        Color NAVY = new Color(0, 0, 128);
+        Color TEAL = new Color(0, 128, 128);
+        Color CORAL = new Color(240, 128, 128);
+        Color KHAKI = new Color(240, 232, 170);
+
         switch (color) {
             case -1 -> graphics.setColor(Color.BLACK);
             case 0 -> graphics.setColor(Color.WHITE);
@@ -74,9 +84,18 @@ public class GridPanel extends JPanel implements ActionListener {
             case 3 -> graphics.setColor(Color.BLUE);
             case 4 -> graphics.setColor(Color.YELLOW);
             case 5 -> graphics.setColor(Color.ORANGE);
-            case 6 -> graphics.setColor(Color.MAGENTA);
-            case 7 -> graphics.setColor(Color.CYAN);
-            case 8 -> graphics.setColor(Color.PINK);
+            case 6 -> graphics.setColor(VIOLET);
+            case 7 -> graphics.setColor(Color.MAGENTA);
+            case 8 -> graphics.setColor(Color.CYAN);
+            case 9 -> graphics.setColor(Color.PINK);
+            case 10 -> graphics.setColor(BROWN);
+            case 11 -> graphics.setColor(LIGHT_GREEN);
+            case 12 -> graphics.setColor(LIGHT_BLUE);
+            case 13 -> graphics.setColor(OLIVE);
+            case 14 -> graphics.setColor(NAVY);
+            case 15 -> graphics.setColor(TEAL);
+            case 16 -> graphics.setColor(CORAL);
+            case 17 -> graphics.setColor(KHAKI);
         }
     }
 }
