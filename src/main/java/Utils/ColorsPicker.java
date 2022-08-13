8994199e81582e3aee921bd78944b47840c9c439
marek <marekkucerabio@gmail.com>
2022-08-13 22:04:01 +0200

    refactoring

---
+++ src/main/java/Utils/ColorsPicker.java
@@ -0,0 +1,44 @@
+package Utils;
+
+import java.awt.*;
+
+public class ColorsPicker {
+    /**
+     * Mapping of grid numbers to colors. Sets {@link Graphics2D} to color represented by a number.
+     *
+     * @param color color number
+     */
+    public static void setColor(Graphics2D graphics, int color) {
+        Color VIOLET = new Color(127, 0, 255);
+        Color BROWN = new Color(139, 69, 19);
+        Color LIGHT_GREEN = new Color(128, 255, 0);
+        Color LIGHT_BLUE = new Color(0, 128, 255);
+        Color OLIVE = new Color(128, 128, 0);
+        Color NAVY = new Color(0, 0, 128);
+        Color TEAL = new Color(0, 128, 128);
+        Color CORAL = new Color(240, 128, 128);
+        Color KHAKI = new Color(240, 232, 170);
+
+        switch (color) {
+            case -1 -> graphics.setColor(Color.BLACK);
+            case 0 -> graphics.setColor(Color.WHITE);
+            case 1 -> graphics.setColor(Color.RED);
+            case 2 -> graphics.setColor(Color.GREEN);
+            case 3 -> graphics.setColor(Color.BLUE);
+            case 4 -> graphics.setColor(Color.YELLOW);
+            case 5 -> graphics.setColor(Color.ORANGE);
+            case 6 -> graphics.setColor(VIOLET);
+            case 7 -> graphics.setColor(Color.MAGENTA);
+            case 8 -> graphics.setColor(Color.CYAN);
+            case 9 -> graphics.setColor(Color.PINK);
+            case 10 -> graphics.setColor(BROWN);
+            case 11 -> graphics.setColor(LIGHT_GREEN);
+            case 12 -> graphics.setColor(LIGHT_BLUE);
+            case 13 -> graphics.setColor(OLIVE);
+            case 14 -> graphics.setColor(NAVY);
+            case 15 -> graphics.setColor(TEAL);
+            case 16 -> graphics.setColor(CORAL);
+            case 17 -> graphics.setColor(KHAKI);
+        }
+    }
+}
--- src/main/java/Windows/GridPanel.java
+++ src/main/java/Windows/GridPanel.java
@@ -1,6 +1,7 @@
 package Windows;
 
 import Logic.Ant;
+import Utils.ColorsPicker;
 import Utils.Settings;
 
 import javax.imageio.ImageIO;
@@ -51,7 +52,7 @@ public class GridPanel extends JPanel implements ActionListener {
     public void draw() {
         for (int row = 0; row < squares; row++) {
             for (int column = 0; column < squares; column++) {
-                setColor(ant.grid[row][column]);
+                ColorsPicker.setColor(graphics, ant.grid[row][column]);
                 graphics.fillRect(row * Settings.SIZE_OF_SQUARE,
                         column * Settings.SIZE_OF_SQUARE,
                         Settings.SIZE_OF_SQUARE,
@@ -66,45 +67,6 @@ public class GridPanel extends JPanel implements ActionListener {
     public void startAnimation() {
         timer = new Timer(Settings.DELAY, this);
         timer.start();
-    }
-
-    /**
-     * Mapping of grid numbers to colors. Sets {@link Graphics2D} to color of current grid tile.
-     *
-     * @param color grid number
-     */
-    private void setColor(int color) {
-        Color VIOLET = new Color(127, 0, 255);
-        Color BROWN = new Color(139, 69, 19);
-        Color LIGHT_GREEN = new Color(128, 255, 0);
-        Color LIGHT_BLUE = new Color(0, 128, 255);
-        Color OLIVE = new Color(128, 128, 0);
-        Color NAVY = new Color(0, 0, 128);
-        Color TEAL = new Color(0, 128, 128);
-        Color CORAL = new Color(240, 128, 128);
-        Color KHAKI = new Color(240, 232, 170);
-
-        switch (color) {
-            case -1 -> graphics.setColor(Color.BLACK);
-            case 0 -> graphics.setColor(Color.WHITE);
-            case 1 -> graphics.setColor(Color.RED);
-            case 2 -> graphics.setColor(Color.GREEN);
-            case 3 -> graphics.setColor(Color.BLUE);
-            case 4 -> graphics.setColor(Color.YELLOW);
-            case 5 -> graphics.setColor(Color.ORANGE);
-            case 6 -> graphics.setColor(VIOLET);
-            case 7 -> graphics.setColor(Color.MAGENTA);
-            case 8 -> graphics.setColor(Color.CYAN);
-            case 9 -> graphics.setColor(Color.PINK);
-            case 10 -> graphics.setColor(BROWN);
-            case 11 -> graphics.setColor(LIGHT_GREEN);
-            case 12 -> graphics.setColor(LIGHT_BLUE);
-            case 13 -> graphics.setColor(OLIVE);
-            case 14 -> graphics.setColor(NAVY);
-            case 15 -> graphics.setColor(TEAL);
-            case 16 -> graphics.setColor(CORAL);
-            case 17 -> graphics.setColor(KHAKI);
-        }
     }
 
     public void saveImage() {
