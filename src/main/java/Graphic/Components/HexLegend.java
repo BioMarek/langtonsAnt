a2b15eaf20ec2233d49a77b76a68686daf35f32e
BioMarek <marekkucerabio@gmail.com>
2023-02-01 21:54:32 +0100

    HexLegend hexes on GridPanel centered

--- src/main/java/Graphic/Components/HexLegend.java
+++ src/main/java/Graphic/Components/HexLegend.java
@@ -51,23 +51,23 @@ public class HexLegend {
 
         graphics.setColor(Colors.TEXT.getColor());
         graphics.draw(new RoundRectangle2D.Double(Settings.LEGEND_START_X + 3.5 * squareSize,
-                squareSize * 3.5,
+                squareSize * 3,
                 squareSize * 3,
                 squareSize * ruleHalf * 2, 20, 20));
 
-        drawLeftArrow(Settings.LEGEND_START_X + squareSize * 19 / 4, squareSize * 7 / 2);
-        drawRightArrow(Settings.LEGEND_START_X + squareSize * 19 / 4, squareSize * 7 / 2 + ruleHalf * gap);
+        drawLeftArrow(Settings.LEGEND_START_X + squareSize * 19 / 4, squareSize * 6 / 2);
+        drawRightArrow(Settings.LEGEND_START_X + squareSize * 19 / 4, squareSize * 6 / 2 + ruleHalf * gap);
 
         for (int i = 0; i < ruleHalf; i++) {
             if (i != ruleHalf - 1)
-                drawDownArrow(Settings.LEGEND_START_X + squareSize * 7 / 2, i * gap + squareSize * 11 / 2);
+                drawDownArrow(Settings.LEGEND_START_X + squareSize * 7 / 2, i * gap + squareSize * 10 / 2);
             drawFillHex(Settings.LEGEND_START_X + squareSize * 3, i * gap + topPadding, i);
             drawLegendInnerArrows(i, squareSize, 3.3, false);
         }
 
         for (int i = ruleHalf; i < hexAnt.ruleLength(); i++) {
             if (i != hexAnt.ruleLength() - 1)
-                drawUpArrow(Settings.LEGEND_START_X + squareSize * 13 / 2, (i - ruleHalf) * gap + squareSize * 11 / 2);
+                drawUpArrow(Settings.LEGEND_START_X + squareSize * 13 / 2, (i - ruleHalf) * gap + squareSize * 10 / 2);
             drawFillHex(Settings.LEGEND_START_X + squareSize * 6, (i - ruleHalf) * gap + topPadding, i);
             drawLegendInnerArrows(i - ruleHalf, squareSize, 6.3, true);
         }
@@ -85,7 +85,7 @@ public class HexLegend {
     private void drawFillHex(int x, int y, int i) {
         x = x + 20;
         int squareSize = Settings.BACKGROUND_HEIGHT / 35;
-        int fillSquareSize = squareSize - 3;
+        int fillSquareSize = squareSize - 4;
 
         // text background for outline
         Polygon hexagon = getHexagon(x, y, squareSize);
